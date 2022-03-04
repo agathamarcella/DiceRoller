c98f7b942100b596a9093ddfa8d6ebb7b90752a4
Agatha Marcella <56811810+agathamarcella@users.noreply.github.com>
2022-03-04 14:13:15 +0700

    Update MainActivity.kt

--- app/src/main/java/com/agathamarcella/diceroller/MainActivity.kt
+++ app/src/main/java/com/agathamarcella/diceroller/MainActivity.kt
@@ -27,6 +27,7 @@ class MainActivity : AppCompatActivity() {
         // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
+        val diceRollSecond = dice.roll()
 
         // Find the ImageView in the layout
         val diceImage: ImageView = findViewById(R.id.imageView)
@@ -46,12 +47,36 @@ class MainActivity : AppCompatActivity() {
 
         // Update the content description
         diceImage.contentDescription = diceRoll.toString()
+
+        // Another Dice
+        // Find the ImageView in the layout
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
+
+        // Determine which drawable resource ID to use based on the dice roll
+        val drawableResource2 = when (diceRollSecond) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Update the ImageView with the correct drawable resource ID
+        diceImage2.setImageResource(drawableResource2)
+
+        // Update the content description
+        diceImage2.contentDescription = diceRoll.toString()
     }
 }
 
 class Dice(val numSides: Int) {
 
     fun roll(): Int {
+        return (1..numSides).random()
+    }
+
+    fun rollSecond(): Int {
         return (1..numSides).random()
     }
 }
